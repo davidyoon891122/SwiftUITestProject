08e74195d375463de06c86e9bf4720b7a7d3951a
David Yoon <davidyoon@hanwha.com>
2023-12-07 16:13:56 +0900

    Initial Commit

---
+++ SwiftUITestProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITestProject
+//
+//  Created by Davidyoon on 2023/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITestProject/SwiftUITestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestProjectApp.swift
+//  SwiftUITestProject
+//
+//  Created by Davidyoon on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
